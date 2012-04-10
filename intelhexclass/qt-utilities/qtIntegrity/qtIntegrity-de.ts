b93e6af36772d19e47eb40e25684a4c13c53e209
Stuart Cording <codinghead@gmail.com>
2012-04-11 00:06:37 +0200

    Created a German translation file for qtIntegrity

---
+++ intelhexclass/qt-utilities/qtIntegrity/qtIntegrity-de.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <source>The &lt;b&gt;qtIntegrity&lt;/b&gt; application analyses the content of an Intel HEX file for correctness. Any errors with the file (such as checksum errors, unknown records, incorrect construction) will be listed. The original file will not be modified. &lt;p&gt;Issues marked &lt;b&gt;ERROR&lt;/b&gt; mean that the file is corrupted and its contents is no longer usable. &lt;p&gt;Issues marked &lt;b&gt;WARNING&lt;/b&gt; highlight issues with the construction of the file that do not affect the binary content, which can still be fully recovered.</source>
+        <translation>Die Anwendung &lt;b&gt;qtIntegrity&lt;\b&gt; analysiert das Inhalt einer HEX-Datei um nach Fehler zu suchen. Alle Fehler, die in der Datei gefunden waren (z.B. Prufsumme-Fehler, unbekannte Felder, fehlhafter Aufbau) werden dargestellt. Die origninale Datei wird nicht geändert. &lt;p&gt;Probeme, die mit &lt;b&gt;Fehler&lt;/b&gt; gekennzeichnet sind, können nicht aufgehoben und heißen, dass die Datei nicht mehr nutzbar ist.&lt;p&gt;Probleme, die mit &lt;b&gt;Warnung&lt;/b&gt; gekennzeichnet sind, sind Problem die behoben werden können, und sind nicht Probleme mit dem Binär Inhalt der Datei. Der Datei ist immer noch zu retten.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="78"/>
+        <source>&amp;Open HEX</source>
+        <translation>&amp;HEX Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="80"/>
+        <source>Analyse Intel HEX file</source>
+        <translation>Intel HEX Datei analysieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>&amp;Save Analysis</source>
+        <translation>&amp;Analyse speichern</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>Save results of analysis</source>
+        <translation>Ergebnis der Analyse speichern</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>E&amp;xit</source>
+        <translation>B&amp;eenden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Exit the application</source>
+        <translation>Diese Applikation schließen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="93"/>
+        <source>&amp;About</source>
+        <translation>&amp;About</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Show the About box</source>
+        <translation>About Meldung öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>About &amp;Qt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation>Qt Biblithek Hilfe öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="117"/>
+        <source>Use File-&gt;Open to open a HEX file to analyse</source>
+        <translation>Wählen Sie Datei-&gt;Öffnen um eine HEX Datei zu analysieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="225"/>
+        <source>qtIntegrity</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="132"/>
+        <source>Cannot read file %1.</source>
+        <translation>Datei %1 konnte nicht aufgemacht werden.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="153"/>
+        <source>No issues found in file %1.</source>
+        <translation>Keine Probleme in %1 gefunden.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="154"/>
+        <location filename="mainwindow.cpp" line="205"/>
+        <source>
+Decode took %1ms.
+</source>
+        <translation>Dekodieren dauerte %1ms.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="158"/>
+        <location filename="mainwindow.cpp" line="209"/>
+        <source>Date and time: </source>
+        <translation>Datum und Uhrzeit:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="161"/>
+        <source>No issues in file</source>
+        <translation>Keine Probleme in dieser Datei gefunden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="167"/>
+        <source>Issues found in file</source>
+        <translation>Dieser Datei enthält Fehler</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="168"/>
+        <source>File %1 contains the following issues:
+</source>
+        <translation>Datei %1 enthält die folgende Fehler:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="183"/>
+        <source>WARNING: </source>
+        <translation>Warnung: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="201"/>
+        <source>ERROR: </source>
+        <translation>Fehler: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="226"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation>Datei %1 ist Schriebgeschutz: %2.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="241"/>
+        <source>Analysis saved</source>
+        <translation>Analyse gespeichert</translation>
+    </message>
+</context>
+</TS>
